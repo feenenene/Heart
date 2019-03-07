ea8b9e1db1519e3f006481310c32a5a3e2a8e2d2
kajiayano <ayanyano428@gmail.com>
2019-03-07 18:43:33 +0900

    add chat

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root 'top#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: %i(index show)
+  resources :reactions, only: %i(create)
+  resources :matching, only: %i(index)
+  resources :chat, only: %i(create show)
 end
